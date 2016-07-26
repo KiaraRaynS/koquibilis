fbde57151650eaea698f6aca6d1a6114bf931e25
KiaraRaynS <sherman.rayn@outlook.com>
2016-07-26 13:27:03 -0400

    Static files fix

--- foodapp/settings.py
+++ foodapp/settings.py
@@ -178,7 +178,6 @@ AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
 
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
 # S3 Stoage
