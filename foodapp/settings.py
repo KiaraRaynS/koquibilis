31648aa713abbd3f63392da0a719784f91e514bc
KiaraRaynS <sherman.rayn@outlook.com>
2016-07-26 13:18:48 -0400

    Static files now linked to AWS S3

--- foodapp/settings.py
+++ foodapp/settings.py
@@ -176,3 +176,10 @@ DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
+# S3 Stoage
+STATICFILES_LOCATION = 'static'
