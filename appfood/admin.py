cc49553bc2a328c34732c259dd71d30cdf571a42
KiaraRaynS <sherman.rayn@outlook.com>
2016-07-31 01:16:53 -0400

    Registered two models to admin and altered fields on Uploads Bookmark

--- appfood/admin.py
+++ appfood/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from appfood.models import Recipe, UserPage, SavedRecipe, ShoppingList
+from appfood.models import Recipe, UserPage, SavedRecipe, ShoppingList, UserUploadedRecipe, UploadedRecipeBookmark
 
 # Register your models here.
 
@@ -22,3 +22,13 @@ admin.site.register(SavedRecipe, SavedRecipeAdmin)
 class ShoppingListAdmin(admin.ModelAdmin):
     list_display = ['user']
 admin.site.register(ShoppingList, ShoppingListAdmin)
+
+
+class UserUploadedRecipeAdmin(admin.ModelAdmin):
+    list_display = ['user', 'title']
+admin.site.register(UserUploadedRecipe, UserUploadedRecipeAdmin)
+
+
+class UploadedRecipeBookmarkAdmin(admin.ModelAdmin):
+    list_display = ['user', 'recipe']
+admin.site.register(UploadedRecipeBookmark, UploadedRecipeBookmarkAdmin)
