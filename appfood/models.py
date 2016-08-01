24fbe3379f8eded1352da9af604db1d1c500fe7f
KiaraRaynS <sherman.rayn@outlook.com>
2016-08-01 09:51:22 -0400

    Changed userhandle to be allowed to be blank

--- appfood/models.py
+++ appfood/models.py
@@ -6,7 +6,7 @@ from django.dispatch import receiver
 
 class UserPage(models.Model):
     user = models.OneToOneField('auth.user')
-    userhandle = models.CharField(max_length=20)
+    userhandle = models.CharField(max_length=20, null=True, blank=True)
     photo = models.ImageField(upload_to='profile_photos', name='photo', blank=True, null=True)
     description = models.CharField(max_length=350, null=True, blank=True)
 
