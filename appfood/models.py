ef75faf455566013e88ef5052b5443a9bba271ac
KiaraRaynS <sherman.rayn@outlook.com>
2016-08-03 13:40:39 -0400

    Recipe images can now be clicked to view individual recipes

--- appfood/models.py
+++ appfood/models.py
@@ -6,7 +6,6 @@ from django.dispatch import receiver
 
 class UserPage(models.Model):
     user = models.OneToOneField('auth.user')
-    userhandle = models.CharField(max_length=20, null=True, blank=True)
     photo = models.ImageField(upload_to='profile_photos', name='photo', blank=True, null=True)
     description = models.CharField(max_length=350, null=True, blank=True)
     bookmarks_private = models.BooleanField(default=False)
