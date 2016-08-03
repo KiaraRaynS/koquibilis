cf792b70b83a6d7c19ad5bd2ea559d80238eae82
KiaraRaynS <sherman.rayn@outlook.com>
2016-08-03 13:14:58 -0400

    Removed user handle

--- appfood/views.py
+++ appfood/views.py
@@ -91,7 +91,7 @@ class RegisterView(CreateView):
 class ProfileView(UpdateView):
     template_name = 'profileview.html'
     model = UserPage
-    fields = ['userhandle', 'photo', 'description', 'bookmarks_private', 'uploads_private']
+    fields = ['photo', 'description', 'bookmarks_private', 'uploads_private']
     success_url = reverse_lazy('profileview')
     authentication_classes = (authentication.TokenAuthentication, SessionAuthentication)
     permission_classes = (IsAuthenticated,)
